d082c2684096d1373fdb32b09a966b4adf7419bb
BautistaLalor <blalor@mail.austral.edu.ar>
2026-03-30 14:31:29 -0300

    Update classify_number.py

--- classify_number.py
+++ classify_number.py
@@ -22,4 +22,13 @@ def classify_number(n):
       - "negative odd"    (negativo e impar)
       - "zero"            (el número es 0)
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    if is_positive(n) and is_even(n):
+        return "positive even"
+    elif is_positive(n) and not is_even(n):
+        return "positive odd"
+    elif not is_positive(n) and is_even(n) and n != 0:
+        return "negative even"
+    elif not is_positive(n) and not is_even(n) and n != 0:
+        return "negative odd"
+    elif n == 0:
+        return "zero"
